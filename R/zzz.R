e6114f43026ab888691da825d3a8928ed3ffb796
Mark Corrales <corrales.mark@epa.gov>
2022-11-20 21:05:30 -0500

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,11 @@
 .onLoad <- function(libname, pkgname) {
   # This should create the index of all US block points to enable fast queries 
   # This cannot be done during package build and saved, because of what this createTree function creates.
+  
+  
+  # NOT TESTED 
+  
+  
   assign(
     "localtree", 
     SearchTrees::createTree(EJAMblockdata::quaddata, treeType = "quad", dataType = "point"), 
