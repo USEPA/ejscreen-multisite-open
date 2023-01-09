88ff54a68c4604a495e04e8809926b963e8488f8
Mark Corrales <corrales.mark@epa.gov>
2023-01-09 18:13:11 -0500

    note how many lines of code are in each package or function

--- inst/0- NOTES_BREAK_EJAM_INTO_FUNCTIONS.R
+++ inst/0- NOTES_BREAK_EJAM_INTO_FUNCTIONS.R
@@ -10,7 +10,7 @@
 #   -Return facility registry IDs (and lat lon also)
 
 # get_siteid_from_sic() ? not urgent
-# get_siteid_from_mact_subpart() ? 
+# get_siteid_from_mact_subpart() ? not urgent
 # 
 # get_siteid_from_programid()  - see EJAMejscreenapi::locate_by_id(type = 'program') or get_facility_info_...
 #   -given program system IDs, 
@@ -55,7 +55,7 @@
 #   -Given NPL ids, or some other IDs that refer to sites with shapefile info? 
 #   -Return shapefile for each  (or sp polygon data) 
 
------------------------------------------------
+# -----------------------------------------------
   
 # NEAR A POINT = INSIDE (CIRCULAR) BUFFER AROUND A POINT:  
 #     sf::st_is_within_distance()   
@@ -69,135 +69,96 @@
 #     sf::st_intersects() to then find points in that buffer, i.e., near the polygon.
 
 
------------------------------------------------
-TO GET RESIDENTS IN A CIRCLE/ NEARBY, OR GET DISTANCE TO EACH 
-
-**EJAM::getblocksnearby() is currently a poorly named function.   see also   sf::st_is_within_distance()  
- possible names:  get_block_distances() ? 
-                  get_blocks_nearby() ? that does not mention distances.
-                  get_distance2blocks() ? _from_sitepoints ?
--Given site lat/lons & max radius (radii?), and table of all US blocks, 
--Return DISTANCES for SITE-BLOCK pairs
-(for each site, and maybe an overall dissolved version that has for each unique block the minimum distance to any site, and average distance if near >1 site)
-
-    get_blocks_in_circle() ?? do we really need this?  see also   sf::st_is_within_distance()  
-    -Given site lat/lons & max radius (radii?), and table of all US blocks 
-    -Return which blockids are nearby (but not distance information? - why not just get distances too?) (for each site, and overall dissolved) 
-
-get_blockpoints_in_shape() - or nearby_blockpoints()  try  sf::st_intersects() to find points inside polygon or buffered polygon.
--Given shapes/buffers as a polygon per site, 
--Return site-block pairs (but not distance to each, just whether inside buffer) (using intersect like ejscreenbatch or others)
-
------------------------------------------------
-TO GET BG WEIGHTS (THE FRACTION OF EACH BLOCKGROUP TO COUNT AS INSIDE OR NEARBY)
-[This could be made even more generic to handle getting fraction of any Census unit (not just bg) at which there is indicator data, like tracts, blocks, raster cells]
-
-** EJAM::doaggregate() is current name for this (and more) roughly:
- ** get_fraction_of_bg_nearby() - if using circular buffer (proximity)
- or essentially get_bgwts_from_blockwts ?
-  ** This is already one part of doaggregate() but could be split off?
-- Given those blockids nearby each site (inside each buffer), and blockwts table,
-- Return BLOCKGROUP WEIGHTS: (for each site, and overall dissolved) list of BLOCKGROUP ids and the weight for each (what fraction of it to count)
-
-get_fraction_of_block_in_shape() ( if areal apportion_blocks)? - see Ejscreenbatch?
--Given buffer shapefiles by site, 
--Return fraction of each BLOCK area that is inside buffer (for dissolved overall shape and/or each buffer). This could be always 0 or 100% if the method specified is to include the whole block population if the block is intersected at all by the buffer. Or it could be a fraction of the block area that falls inside the buffer. 
-- THEN NEED FUNCTION TO ROLL THAT UP to return the fraction of each blockgroup, since that is the resolution at which you have indicators typically.
-  
-get_fraction_of_bg_in_shape()  - or arealapportion_bg()? see ejscreenbatch
--Given buffer shapefiles by site, 
--Return BLOCKGROUP ids and the fraction of each BLOCKGROUP area that is inside buffer (for dissolved overall shape and/or each site or buffer). This could be always 0 or 100% if the method specified is to include the whole block population if the block is intersected at all by the buffer. Or it could be a fraction of the block area that falls inside the buffer.
-
-get_fraction_of_rastercell_in_shape()  - see ejscreenbatch
--Given buffer shapefiles by site, 
--Return which RASTER CELLS, or what fraction of each, are inside buffer (for dissolved overall shape and/or each buffer). This could be always 0 or 100% if the method specified is to include the whole block population if the block is intersected at all by the buffer. Or it could be a fraction of the block area that falls inside the buffer. This presumes you already have indicators at the raster cell scale, and do not have to translate the raster cells or fractions into some other units like blockgroups.
-   
------------------------------------------------
-TO CALCULATE BG INDICATOR AGGREGATED FOR EACH SITE AND OVERALL- JOIN BLOCKGROUP INDICATOR DATA and BG WTS and use formulas like ratios of sums of counts to get %poor, etc.
-
-** EJAM::doaggregate() is current name for this (and more) roughly:
- ** aggregate_indicators() -- takes BG scores and BG wts and use formulas to aggregate by site and overall.
--Given blockgroup weights nearby or in buffers (by site and overall), blockgroupstats (any indicators, for each blockgroup in US), formulas or metadata on types of indicators (how to aggregate them), etc., 
--Return each indicators (by site and overall) as raw values only  (percentiles lookup separately)
- **this could be broken up into 3 helper functions
-   * rollup_counts(), 
-   * rollup_means(), (can be wtd by pop, or by area, or by census block?)
-   * rollup_formulas() or  calculate_by_formula(), 
-
-** EJAM::doaggregate() is current name for this (and more) roughly:
- ** percentiles_from_raw() - this is maybe in lookup.pctile() already?
--Given raw values of indicators, percentiles lookup tables,
--Return percentiles (US and/or State)
-
------------------------------------------------
-  OTHER
-
-
-** popup_from_df()  or more complicated/tailored version:
-  Get popups text from percentiles (see ejscreen::make.popups.api() or something like that)
-- given percentiles for indicators
-- return popup text spelling that out for map popup
-
-mapfast()  or maybe map_sites()  
- 
-
-prep for excel: excel-related formatting of outputs 
-
-** batch.summarize() ... break up #   **350** lines of code  
-batch.clean() .... should be broken up; 
-for summary rows and columns; functions for print tables?
-  
-** write_report() - a bit of this is in the exec sum tab of EJAMbatch.summarizer::
-  
-ratios_to_reference() ? 
--Given raw values of indicators, and US/State overall means
--Return those as ratios to US (and State?) overall means. 
-
-
-plot_boxplots() ratios?
-plot_barplots_?? ratios?
-
-
------------------------
-
-
-# server.R  had     **1700** lines of code in batch.summarizer package
+# -----------------------------------------------
+# TO GET RESIDENTS IN A CIRCLE/ NEARBY, OR GET DISTANCE TO EACH 
 # 
-  z = analyze.stuff::linesofcode('..', packages = 'EJAM')
-  z[order(z$lines, decreasing = T), 1:5]
-    lines code comments package                                                               filename
-    
-48    775  761       14    EJAM                                                               app_ui.R
-53    575  522       53    EJAM                                                          doaggregate.R *******
-47    529  523        6    EJAM                                                           app_server.R *****
+# **EJAM::getblocksnearby() is currently a poorly named function.   see also   sf::st_is_within_distance()  
+#  possible names:  get_block_distances() ? 
+#                   get_blocks_nearby() ? that does not mention distances.
+#                   get_distance2blocks() ? _from_sitepoints ?
+# -Given site lat/lons & max radius (radii?), and table of all US blocks, 
+# -Return DISTANCES for SITE-BLOCK pairs
+# (for each site, and maybe an overall dissolved version that has for each unique block the minimum distance to any site, and average distance if near >1 site)
+# 
+#     get_blocks_in_circle() ?? do we really need this?  see also   sf::st_is_within_distance()  
+#     -Given site lat/lons & max radius (radii?), and table of all US blocks 
+#     -Return which blockids are nearby (but not distance information? - why not just get distances too?) (for each site, and overall dissolved) 
+# 
+# get_blockpoints_in_shape() - or nearby_blockpoints()  try  sf::st_intersects() to find points inside polygon or buffered polygon.
+# -Given shapes/buffers as a polygon per site, 
+# -Return site-block pairs (but not distance to each, just whether inside buffer) (using intersect like ejscreenbatch or others)
 
-44    243  243        0    EJAM                                          ejscreenapi-make.popups.api.R  **??
-56    207  158       49    EJAM                                                      getacs_epaquery.R 
-11    202  186       16    EJAM                                                          cleanoutput.R ?
 
-61    161  147       14    EJAM                                 getblocksnearbyviaQuadTree_Clustered.R
-62    144  109       35    EJAM                                          getblocksnearbyviaQuadTree2.R
-60    142  106       36    EJAM                                           getblocksnearbyviaQuadTree.R 
+# -----------------------------------------------
+# TO GET BG WEIGHTS (THE FRACTION OF EACH BLOCKGROUP TO COUNT AS INSIDE OR NEARBY)
+# [This could be made even more generic to handle getting fraction of any Census unit (not just bg) at which there is indicator data, like tracts, blocks, raster cells]
+# 
+# ** EJAM::doaggregate() is current name for this (and more) roughly:
+#  ** get_fraction_of_bg_nearby() - if using circular buffer (proximity)
+#  or essentially get_bgwts_from_blockwts ?
+#   ** This is already one part of doaggregate() but could be split off?
+# - Given those blockids nearby each site (inside each buffer), and blockwts table,
+# - Return BLOCKGROUP WEIGHTS: (for each site, and overall dissolved) list of BLOCKGROUP ids and the weight for each (what fraction of it to count)
+# 
+# get_fraction_of_block_in_shape() ( if areal apportion_blocks)? - see Ejscreenbatch?
+# -Given buffer shapefiles by site, 
+# -Return fraction of each BLOCK area that is inside buffer (for dissolved overall shape and/or each buffer). This could be always 0 or 100% if the method specified is to include the whole block population if the block is intersected at all by the buffer. Or it could be a fraction of the block area that falls inside the buffer. 
+# - THEN NEED FUNCTION TO ROLL THAT UP to return the fraction of each blockgroup, since that is the resolution at which you have indicators typically.
+#   
+# get_fraction_of_bg_in_shape()  - or arealapportion_bg()? see ejscreenbatch
+# -Given buffer shapefiles by site, 
+# -Return BLOCKGROUP ids and the fraction of each BLOCKGROUP area that is inside buffer (for dissolved overall shape and/or each site or buffer). This could be always 0 or 100% if the method specified is to include the whole block population if the block is intersected at all by the buffer. Or it could be a fraction of the block area that falls inside the buffer.
+# 
+# get_fraction_of_rastercell_in_shape()  - see ejscreenbatch
+# -Given buffer shapefiles by site, 
+# -Return which RASTER CELLS, or what fraction of each, are inside buffer (for dissolved overall shape and/or each buffer). This could be always 0 or 100% if the method specified is to include the whole block population if the block is intersected at all by the buffer. Or it could be a fraction of the block area that falls inside the buffer. This presumes you already have indicators at the raster cell scale, and do not have to translate the raster cells or fractions into some other units like blockgroups.
+#    
 
+# -----------------------------------------------
+# TO CALCULATE BG INDICATOR AGGREGATED FOR EACH SITE AND OVERALL- JOIN BLOCKGROUP INDICATOR DATA and BG WTS and use formulas like ratios of sums of counts to get %poor, etc.
+# 
+# ** EJAM::doaggregate() is current name for this (and more) roughly:
+#  ** aggregate_indicators() -- takes BG scores and BG wts and use formulas to aggregate by site and overall.
+# -Given blockgroup weights nearby or in buffers (by site and overall), blockgroupstats (any indicators, for each blockgroup in US), formulas or metadata on types of indicators (how to aggregate them), etc., 
+# -Return each indicators (by site and overall) as raw values only  (percentiles lookup separately)
+#  **this could be broken up into 3 helper functions
+#    * rollup_counts(), 
+#    * rollup_means(), (can be wtd by pop, or by area, or by census block?)
+#    * rollup_formulas() or  calculate_by_formula(), 
+# 
+# ** EJAM::doaggregate() is current name for this (and more) roughly:
+#  ** percentiles_from_raw() - this is maybe in lookup.pctile() already?
+# -Given raw values of indicators, percentiles lookup tables,
+# -Return percentiles (US and/or State)
+# 
 
-z = linesofcode('..', packages = 'EJAMejscreenapi')
-z[order(z$code, decreasing = T), 1:5]
-    lines code comments         package                                            filename
-    
-53   570  569        1 EJAMejscreenapi                                                ui.R
-52   517  517        0 EJAMejscreenapi                                            server.R *****
-
-23   398  355       43 EJAMejscreenapi                                  ejscreenapi_plus.R  
-22   312  272       40 EJAMejscreenapi                                       ejscreenapi.R 
-24   271  168      103 EJAMejscreenapi                                ejscreenapi_script.R 
-
-41   317  291       26 EJAMejscreenapi                                   make.popups.api.R  ?? but also see popup_from_df()
-
-33   215  163       52 EJAMejscreenapi                        get_facility_info_via_ECHO.R  **but rename?
-
-4    271  130      141 EJAMejscreenapi                            usefulcolumns_ECHO_API.R
-51   143  112       31 EJAMejscreenapi                                     varname2color.R
-40   225  108      117 EJAMejscreenapi                                      locate_by_id.R **
-25   139  107       32 EJAMejscreenapi                                      ejscreenapi1.R 
-
-  
+# -----------------------------------------------
+#   OTHER
+# 
+# ** popup_from_df()  or more complicated/tailored version:
+#   Get popups text from percentiles (see ejscreen::make.popups.api() or something like that)
+# - given percentiles for indicators
+# - return popup text spelling that out for map popup
+# 
+# mapfast()  or maybe map_sites()  
+#  
+# 
+# prep for excel: excel-related formatting of outputs 
+# 
+# ** batch.summarize() ... break up #   **350** lines of code  
+# batch.clean() .... should be broken up; 
+# for summary rows and columns; functions for print tables?
+#   
+# ** write_report() - a bit of this is in the exec sum tab of EJAMbatch.summarizer::
+#   
+# ratios_to_reference() ? 
+# -Given raw values of indicators, and US/State overall means
+# -Return those as ratios to US (and State?) overall means. 
+# 
+# 
+# plot_boxplots() ratios?
+# plot_barplots_?? ratios?
+# 
+# 
+# -----------------------
+#  
---
+++ inst/how_many_lines_of_code_are_here.R
@@ -0,0 +1,159 @@
+
+# HOW MANY LINES OF CODE (AND COMMENTS) ARE IN THESE PACKAGES (on your local drive)
+#  IN .R FILES
+#  IN ANY FOLDER OR JUST IN THE /R/ FOLDER?
+
+# https://github.com/ejanalysis/analyze.stuff/blob/master/R/linesofcode.R
+
+
+#  analyze.stuff::linesofcode('..', packages = EJAM::ejampackages )
+## Including root and inst folders, etc.:
+#               package filecount lines
+# 1                 EJAM      115 12044
+# 2 EJAMbatch.summarizer       61  7137
+# 3      EJAMejscreenapi       70  6415
+# 4          EJAMfrsdata       20  1213
+# 5        EJAMblockdata        9   570
+# 6     EJAMejscreendata        9   435
+
+
+
+# >     x <-      linesofcode('..', packages = EJAM::ejampackages,  rfolderonly = TRUE)
+#
+#               package filecount lines
+# 1 EJAMbatch.summarizer       45  5579
+# 2                 EJAM       58  4890
+# 3      EJAMejscreenapi       46  3499
+# 4          EJAMfrsdata       16   863
+# 5        EJAMblockdata        8   268
+# 6     EJAMejscreendata        5   257
+#
+# Most of the code is in these files:
+#
+#   lines comments code              package where                           filename
+# 1   1691        8 1683 EJAMbatch.summarizer   /R/                       app_server.R
+# 3   1084       16 1068 EJAMbatch.summarizer   /R/                           app_ui.R
+# 4    767       15  752                 EJAM   /R/                           app_ui.R
+# 6    575       53  522                 EJAM   /R/                      doaggregate.R
+# 9    541        8  533                 EJAM   /R/                       app_server.R
+# 12   405      170  235                 EJAM   /R/                   golem_utils_ui.R
+# 13   380       27  353 EJAMbatch.summarizer   /R/                  batch.summarize.R
+# 15   328       27  301      EJAMejscreenapi   /R/                  make.popups.api.R
+# 16   325       47  278      EJAMejscreenapi   /R/                      ejscreenapi.R
+# 19   281        9  272 EJAMbatch.summarizer   /R/ ascript_for_now_to_get_US_TOTALS.R
+# 22   256       96  160      EJAMejscreenapi   /R/               ejscreenapi_script.R
+# 26   231       83  148 EJAMbatch.summarizer   /R/                        ustotals2.R
+# 27   228      119  109      EJAMejscreenapi   /R/                     locate_by_id.R
+# 29   221      133   88      EJAMejscreenapi   /R/                get.distances.all.R
+# 30   216       52  164      EJAMejscreenapi   /R/       get_facility_info_via_ECHO.R
+# 33   207       49  158                 EJAM   /R/                  getacs_epaquery.R
+
+#
+# Most of the code is in these files:
+#
+#    lines comments code              package                   where                                    filename
+# 1   1691        8 1683 EJAMbatch.summarizer                     /R/                                app_server.R
+
+# 3   1084       16 1068 EJAMbatch.summarizer                     /R/                                    app_ui.R
+# 4    767       15  752                 EJAM                     /R/                                    app_ui.R
+# 5    580        2  578      EJAMejscreenapi                       /   ***                                 server.R
+# 6    575       53  522                 EJAM                     /R/                               doaggregate.R
+# 7    575        7  568      EJAMejscreenapi                       /    ***                                    ui.R
+
+# 9    541        8  533                 EJAM                     /R/                                app_server.R
+
+# 12   405      170  235                 EJAM                     /R/                            golem_utils_ui.R
+# 13   380       27  353 EJAMbatch.summarizer                     /R/                           batch.summarize.R
+
+# 15   328       27  301      EJAMejscreenapi                     /R/                           make.popups.api.R
+# 16   325       47  278      EJAMejscreenapi                     /R/                               ejscreenapi.R
+
+# 18   281        9  272 EJAMbatch.summarizer                     /R/          ascript_for_now_to_get_US_TOTALS.R
+# 19   277      148  129      EJAMejscreenapi              /data-raw/  ***                varsinfo_ECHO_API_script.R
+# 20   269      149  120 EJAMbatch.summarizer                  /inst/  ***                                  global.R
+# 21   256       96  160      EJAMejscreenapi                     /R/                        ejscreenapi_script.R
+
+# 25   231       83  148 EJAMbatch.summarizer                     /R/                                 ustotals2.R
+# 26   228      119  109      EJAMejscreenapi                     /R/                              locate_by_id.R
+
+# 28   221      133   88      EJAMejscreenapi                     /R/                         get.distances.all.R
+# 29   216       52  164      EJAMejscreenapi                     /R/                get_facility_info_via_ECHO.R
+#
+# Full list is returned invisibly
+
+
+
+
+
+#
+# > sapply(EJAM::ejampackages, FUN = function(ppp) linesofcode('..',packages = ppp, rfolderonly = TRUE))
+#
+#   package filecount lines
+# 1    EJAM       58  4890
+# Most of the code is in these files:
+#
+#   lines comments code package where          filename
+# 2    767       15  752    EJAM   /R/          app_ui.R
+# 3    575       53  522    EJAM   /R/     doaggregate.R
+# 4    541        8  533    EJAM   /R/      app_server.R
+# 7    405      170  235    EJAM   /R/  golem_utils_ui.R
+# 14   207       49  158    EJAM   /R/ getacs_epaquery.R
+
+
+
+#                package filecount lines
+# 1 EJAMbatch.summarizer       45  5579
+# Most of the code is in these files:
+#
+#   lines comments code              package where          filename
+# 1  1691        8 1683 EJAMbatch.summarizer   /R/      app_server.R
+# 2  1084       16 1068 EJAMbatch.summarizer   /R/          app_ui.R
+# 3   380       27  353 EJAMbatch.summarizer   /R/ batch.summarize.R
+
+
+#         package filecount lines
+# 1 EJAMblockdata        8   268
+# Most of the code is in these files:
+#
+#   lines comments code       package where            filename
+# 2    77       75    2 EJAMblockdata   /R/ blockdata-package.R
+# 3    46       45    1 EJAMblockdata   /R/          blockwts.R
+# 4    43       42    1 EJAMblockdata   /R/          quaddata.R
+
+
+#           package filecount lines
+# 1 EJAMejscreenapi       46  3499
+# Most of the code is in these files:
+#
+#   lines comments code         package where                     filename
+# 4    328       27  301 EJAMejscreenapi   /R/            make.popups.api.R
+# 5    325       47  278 EJAMejscreenapi   /R/                ejscreenapi.R
+# 7    256       96  160 EJAMejscreenapi   /R/         ejscreenapi_script.R
+# 8    228      119  109 EJAMejscreenapi   /R/               locate_by_id.R
+# 9    221      133   88 EJAMejscreenapi   /R/          get.distances.all.R
+# 10   216       52  164 EJAMejscreenapi   /R/ get_facility_info_via_ECHO.R
+# 12   144       34  110 EJAMejscreenapi   /R/               ejscreenapi1.R
+# 13   144       32  112 EJAMejscreenapi   /R/              varname2color.R
+
+
+#            package filecount lines
+# 1 EJAMejscreendata        5   257
+# Most of the code is in these files:
+#
+#   lines comments code          package where                               filename
+# 3    64       63    1 EJAMejscreendata   /R/                          States_2022.R
+# 4    58       57    1 EJAMejscreendata   /R/ EJSCREEN_StatePct_with_AS_CNMI_GU_VI.R
+# 5    52       51    1 EJAMejscreendata   /R/     EJSCREEN_Full_with_AS_CNMI_GU_VI.R
+
+
+#       package filecount lines
+# 1 EJAMfrsdata       16   863
+# Most of the code is in these files:
+#
+#   lines comments code     package where                    filename
+# 2   185      128   57 EJAMfrsdata   /R/                   frs_get.R
+# 3   116       40   76 EJAMfrsdata   /R/                  frs_read.R
+# 4    80        7   73 EJAMfrsdata   /R/ frs_make_programid_lookup.R
+# 5    77        9   68 EJAMfrsdata   /R/     frs_make_naics_lookup.R
+#
+# Full list is returned invisibly
