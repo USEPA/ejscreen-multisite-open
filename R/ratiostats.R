4b218d2fb0f6c36dac0dce504ae457d99e026e3b
Mark Corrales <corrales.mark@epa.gov>
2023-03-03 04:02:05 -0500

    ratiostats() is another way to get each Demog group's ratio of score to non-D avg, for distances, proximity score, any Env indicator.
    but relies on a function in ejanalysis package that I don't want to require

---
+++ R/ratiostats.R
@@ -0,0 +1,59 @@
+
+ratiostats <- function(results_bybg_people, pts, dig=2 ) {
+  
+  # Comparison of Indicator scores for Average person in each Demog Group
+  
+  stinfo <- data.frame(siteid=1:nrow(pts),  state_from_latlon(lat=pts$lat, lon=pts$lon))
+  # x <- getblocksnearby(pts, cutoff = 3.1, quadtree = localtree)
+  # y <- doaggregate(x, sites2states = stinfo)
+  # z <- y$results_bybg_people
+  z <- results_bybg_people # a data.table from  doaggregate()[,"results_bybg_people"]
+  
+  vars <- c("proximityscore", "sitecount_max", "sitecount_near_bg", names_e)
+  vars2 <- c("distance_avg", "sitedistance_min")
+  vars <- c(vars, vars2)
+  
+  # other functions that do similar things
+  # setDF(z)
+  # r2 <- RR.table(z, vars, c(names_d, names_d_subgroups))
+  # r2 <- RR(e=z[ , ..vars], d= z[ ,  c(..names_d, ..names_d_subgroups)] , pop=z$pop)
+  #
+  r2 <- ejanalysis::RR.means(e=z[ , ..vars], d= z[ ,  c(..names_d, ..names_d_subgroups)] , pop=z$pop) 
+  r2 <- r2[,"ratio",]
+  # essentially for each group it is just =  colSums(x * groupwts) / colSums(groupwts) with na.rm=T
+   #  divided by the same formula for everyone else
+ 
+  
+  r2 <- addmargins(r2, FUN=list(max = function(x) 1.0001 * max(x)) ) # to help sort max row or col 1st before actual max
+  r2 <- r2[order(r2[,"max"],decreasing = T), order(r2["max",],decreasing = T)]
+  r2 <- round(r2,dig)
+  return(r2)
+  
+  
+  # plot()
+  
+  # library(EJAM)
+  # library(EJAMfrsdata)
+  # library(EJAMblockdata
+  #         
+  # pts <- EJAMfrsdata::frs[sample(1:nrow(EJAMfrsdata::frs), 1e3),]
+  
+  
+  # > dimnames(r2) when 1st created:
+  # $d
+  # [1] "VSI.eo"          "pctlowinc"       "pctmin"          "pctlths"         "pctlingiso"      "pctunder5"      
+  # [7] "pctover64"       "pctunemployed"   "pctnhwa"         "pcthisp"         "pctnhba"         "pctnhaa"        
+  # [13] "pctnhaiana"      "pctnhnhpia"      "pctnhotheralone" "pctnhmulti"     
+  # 
+  # $group.or.not
+  # [1] "group" "not"   "ratio"
+  # 
+  # $e
+  # [1] "proximityscore"    "distance_avg"      "sitedistance_min"  "sitecount_max"     "sitecount_near_bg"
+  # [6] "pm"                "o3"                "cancer"            "resp"              "dpm"              
+  # [11] "pctpre1960"        "traffic.score"     "proximity.npl"     "proximity.rmp"     "proximity.tsdf"   
+  # [16] "proximity.npdes"   "ust"              
+}
+
+
+
