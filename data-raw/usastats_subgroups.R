eb7d2cef75c11ec83eab711f3e0b5ac7e9b264a0
Mark Corrales <corrales.mark@epa.gov>
2023-03-13 12:59:57 -0400

    state_from_..., states_infer(), updated lookup tables statestats /usastats, test data, etc.

--- data-raw/usastats_subgroups.R
+++ data-raw/usastats_subgroups.R
@@ -1,29 +1,53 @@
 
-# TO BE CONTINUED....  NOT DONE WITH THIS YET....  
+# This is a script used to add columns to usastats and statestats with info on demographic subgroups
+
 
 # Create percentile lookup tables for extra variables like demographic race/ethnicity subgroups
 bg <- data.table::copy(EJAM::blockgroupstats)
-bg = setDF(bg)
-bg = addFIPScomponents(bg, fipscolname = "bgfips",  clean = FALSE)
+bg <- data.table::setDF(bg)
+# bg = ejanalysis::addFIPScomponents(bg, fipscolname = "bgfips",  clean = FALSE)
+bg$FIPS.ST <- substr(bg$bgfips,1,2)
+bg$ST <- EJAM::stateinfo$ST[match(bg$FIPS.ST, EJAM::stateinfo$FIPS.ST)]
 
 
    usastats_subgroups   <- write_pctiles_lookup(data.frame(bg)[ , names_d_subgroups])
-   usastats_subgroups <- rbind(NA, usastats_subgroups); usastats_subgroups$PCTILE[1] <- 0
-   # but...
+   usastats_subgroups <- rbind(0, usastats_subgroups); usastats_subgroups$PCTILE[1] <- 0
+   usastats_subgroups[1, c("OBJECTID", "REGION")] <- c(0,"USA")
+
+   
    statestats_subgroups <- write_pctiles_lookup(data.frame(bg)[ , names_d_subgroups], zone.vector = bg$ST)
-   # not as easy to create 0th row for each state here...
-   # not as easy to merge this with existing, either....  to be completed later..,, 
-   
-   # THOSE FAIL TO CREATE THE ZERO PERCENTILE ROW:    **********   FIX METHOD TO MATCH CURRENT EJScreen method, or fit rows in with merge?
-   
+    zerorowperstate <- data.frame(
+     OBJECTID=0,
+     REGION=unique(statestats_subgroups$REGION),
+     PCTILE=0,
+     pctnhwa=0, pcthisp=0, pctnhba=0, pctnhaa=0, pctnhaiana=0, pctnhnhpia=0, pctnhotheralone=0,pctnhmulti=0
+     )
+   statestats_subgroups <- rbind(statestats_subgroups, zerorowperstate)
+   statestats_subgroups <- statestats_subgroups[order(statestats_subgroups$REGION, as.numeric(statestats_subgroups$PCTILE)), ]
+   statestats_subgroups$OBJECTID <- 1:NROW(statestats_subgroups)
    # > dim(usastats)
    # [1] 103  35
    # > dim(usastats_subgroups)
-   # [1] 102  11     MISSING THE PCTILE == 0  ROW
+   # [1] 103  11     
    
  
    usastats2 <- cbind(usastats, usastats_subgroups[, setdiff(names(usastats_subgroups), names(usastats))  ])
  
-   # not tried yet:
   statestats2 <- merge(statestats, statestats_subgroups, all.x=TRUE, all.y=FALSE, by= c("PCTILE", "REGION"))
+statestats2$OBJECTID.x <- NULL
+statestats2$OBJECTID.y <- NULL
+statestats2 <- statestats2[order(statestats2$REGION, as.numeric(statestats2$PCTILE)), ]
+statestats2$OBJECTID <- 1:NROW(statestats2)
+rownames(statestats2) <- 1:NROW(statestats2)
+statestats2 <- statestats2[, c(names(statestats), names_d_subgroups)]
 
+statestats2                              <- EJAM::metadata_add(statestats2)
+usastats2                           <- EJAM::metadata_add(usastats2)
+
+usastats <- usastats2
+data.table::setDF(usastats) # keep as data.frame actually
+usethis::use_data(usastats, overwrite = TRUE)
+
+statestats <- statestats2
+data.table::setDF(statestats) # keep as data.frame actually
+usethis::use_data(statestats, overwrite = TRUE)
