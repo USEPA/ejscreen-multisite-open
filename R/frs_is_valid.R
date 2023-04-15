07a33e20e0020bf1bb18f0f13f9586636ab2c94f
Mark Corrales <corrales.mark@epa.gov>
2023-04-15 01:39:47 -0400

    fix select by file with FRS Registry IDs

--- R/frs_is_valid.R
+++ R/frs_is_valid.R
@@ -1,21 +1,36 @@
-#' Validate FRS uploads
+#' Validate FRS Registry ID list uploads
 #' @description Check for proper FRS facility id in uploaded data
 #'
-#' @param frs_upload upload frs converted to data frame
+#' @param frs_upload upload frs registry IDs table converted to data frame
 #' @return boolean value (valid or not valid)
 #' @export
 #'
 frs_is_valid <- function(frs_upload) {
   #checks for registry id in second column of FRS uploaded dataset
-  if(any(colnames(frs_upload) %in% "REGISTRY_ID")){
-   # for (i in frs_upload$REGISTRY_ID) {
-      #if(nchar(i) != 12){ #is every registry ID the correct length
-        #return(FALSE)
-      #}
-      
+  if(any(colnames(frs_upload) %in%  "REGISTRY_ID" )) {  # FRS uses this colname
+    # for (i in frs_upload$REGISTRY_ID) {
+    #if(nchar(i) != 12){ #is every registry ID the correct length
+    #return(FALSE)
+    #}
     #}
     return(TRUE)
   } else {
-    return(FALSE)
+    if ("regid" %in% colnames(frs_upload)) {
+      colnames(frs_upload) <- gsub("RegistryID", "REGISTRY_ID", colnames(frs_upload)) # ECHO uses this colname
+      return(TRUE)
+    } else {
+      if ("siteid" %in% colnames(frs_upload)) {
+        colnames(frs_upload) <- gsub("siteid", "REGISTRY_ID", colnames(frs_upload))
+        warning("assuming siteid column has REGISTRY_ID values")
+        return(TRUE)
+      } else {
+        if ("siteid" %in% colnames(frs_upload)) {
+          colnames(frs_upload) <- gsub("siteid", "REGISTRY_ID", colnames(frs_upload))
+          warning("assuming siteid column has REGISTRY_ID values")
+          return(TRUE)
+        }
+      }
+    } 
   }
+  return(FALSE)
 }
