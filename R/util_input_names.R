bfcfb51400b9809a085182742da3904c9adb2a1b
Mark Corrales <corrales.mark@epa.gov>
2023-04-16 19:53:55 -0400

    checking names of input$____ parameters

---
+++ R/util_input_names.R
@@ -0,0 +1,16 @@
+#' input_names_listing
+#' Utility checking values of input$ that appear in this code
+#' @param file  path to source file to search in 
+#'
+#' @return character vector of ids of inputs like x,y,z if it found input$x input$y input$z
+#' @export
+input_names_listing <- function(file="./R/app_server.R") {
+  x = readLines(file)
+  
+  cat("Found these input$____  values in that source file ",file,"  input$____ :\n\n")
+  # this is not perfect, but roughly gets the list of inputs:
+  sort(
+    gsub("input\\$", "", unique(grep("input\\$", gsub("(.*)(input\\$[A-Za-z0-9_-]*)(.*)","\\2", x), value=T)))
+  )
+}
+
