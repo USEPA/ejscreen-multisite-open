33a7dfcc90c33064770dfaa6e92763b25879239f
Mark Corrales <corrales.mark@epa.gov>
2023-04-18 11:57:11 -0400

    formatting and documentation changes only

--- R/aaa_onLoad_create_quad_tree_index.R
+++ R/aaa_onLoad_create_quad_tree_index.R
@@ -8,7 +8,7 @@
   # This should create the index of all US block points to enable fast queries 
   # This cannot be done during package build and saved, because of what this createTree function creates.
   # NOT TESTED in context of an app published on RStudio Server
-  cat("Building Index of Census Blocks (localtree)...\n")
+  cat("Building index of Census Blocks (localtree)...\n")
   if (!exists("localtree")) {
     
   assign(
@@ -18,24 +18,28 @@
     # need to test, but seems to work. 
     # But takes a couple seconds at every reload of pkg.
   )
-  cat("Done building index.\n")
+  cat("  Done building index.\n")
   }
   
   
   if (!interactive()) {
   ### Maybe load the datasets now that are needed, 
   ### instead of when user does a query that triggers slow lazyloading?
-  cat("Loading data.tables of Census Blocks (blockpoints, blockwts)\n")
+  cat("Loading data.tables of Census Blocks...\n")
   data("blockgroupstats") # data(EJAM::blockgroupstats)  # 54 MB rda file
   #  statestats.rda is about 1.3 MB, usastats.rda is about 28 KB
-  data(blockpoints, package="EJAMblockdata"); cat('blockpoints loaded\n')  
-  data(blockwts,    package="EJAMblockdata"); cat('blockwts loaded\n') 
+  data(blockpoints, package="EJAMblockdata"); cat('  Done loading blockpoints.\n')  
+  data(blockwts,    package="EJAMblockdata"); cat('  Done loading blockwts.\n') 
   # data(EJAMfrsdata::frs) # will be lazy loaded if/when needed
   
   } else {
-    cat("You may want to modify this .onLoad() function, to have it immediately load data (which takes several seconds) instead of that happening only when you first run an analysis, ") 
-    cat("as with quaddata (168 MB), blockgroupstats  (54 MB), blockpoints (86 MB) and blockwts (31 MB), blockid2fips (20 MB) files, ")
-    cat("data.tables of Census Blocks or Block Groups.\n")
+    cat("Developers may want to modify the .onLoad() function, \n")
+    cat("to have it immediately load less or more data \n")
+    cat("(which takes several seconds each time you rebuild or just load the package)\n") 
+    cat("instead of that happening only when you first run an analysis,\n") 
+    cat("as with these large datasets (data.tables of Census blocks or block groups):\n ")
+    cat("\n- quaddata (168 MB), \n- blockgroupstats  (54 MB), \n- blockpoints (86 MB), \n- blockwts (31 MB), \n- blockid2fips (20 MB)\n ")
+    cat("\n")
   }
 
 }
