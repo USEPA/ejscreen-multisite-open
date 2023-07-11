491029ccd04c914ca8a31a4dc889aeb9de7f1751
Mark Corrales <corrales.mark@epa.gov>
2023-07-11 14:07:20 -0400

    ejscreenapi module to access the EJScreen API for a batch of sites

--- R/app_ui.R
+++ R/app_ui.R
@@ -1003,6 +1003,9 @@ tabPanel(title = 'Details',
                # - hidden by default but can be activated by a button (see About EJAM tab)
                tabPanel(title = 'Advanced Settings',
                         
+                        ## Get ejscreen reports via API ####
+                        mod_ejscreenapi_ui("ejscreenapi_1"),
+                        
                         ## input: Type of plot for 1page report
                         shiny::radioButtons(inputId = "plotkind_1pager", 
                                             label = "Type of plot for 1page report",
