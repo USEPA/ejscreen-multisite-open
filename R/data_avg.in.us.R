70f5bc492c590703b38bf3657eb9b8ac28d14aed
Mark Corrales <corrales.mark@epa.gov>
2023-10-24 10:08:14 -0400

    update avg.in.us

--- R/data_avg.in.us.R
+++ R/data_avg.in.us.R
@@ -1,5 +1,5 @@
 #' @name avg.in.us
 #' @docType data
 #' @title avg.in.us (DATA) national averages of key indicators, for convenience
-#' @description also available via [usastats]
+#' @description also available via [usastats] and created by /data-raw/datacreate_avg.in.us
 NULL
--- data-raw/datacreate_avg.in.us.R
+++ data-raw/datacreate_avg.in.us.R
@@ -3,6 +3,8 @@
 # morenames <- c(names_these, names_d_subgroups_alone, names_d_subgroups_nh ) # not raw EJ indexes though?
 avg.in.us <-   usastats[ usastats$PCTILE == "mean", intersect(names_these, names( usastats))]
 
+avg.in.us <- metadata_add(avg.in.us)
+
 # note the regular name not avg. name is used in the usastats table
 usethis::use_data(avg.in.us, overwrite = TRUE)
 
