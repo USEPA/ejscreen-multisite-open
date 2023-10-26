615496cdb9eafaf09410f6c8da1d16710e3b58d6
Mark Corrales <corrales.mark@epa.gov>
2023-10-25 20:07:37 -0400

    Create fixmapheadernamescolname.R

---
+++ R/fixmapheadernamescolname.R
@@ -0,0 +1,23 @@
+
+#' fixmapheadernamescolname - utility to convert aliases to proper colnames of map_headernames
+#'
+#' @param x character vector of colnames of map_headernames, or aliases like "long"
+#'
+#' @return vector where aliases are replaced with actual colnames and unmatched ones left as-is
+#' @export
+#'
+#' @examples 
+#'   fixmapheadernamescolname(c('long', 'csv', 'api', 'r'))
+#' 
+fixmapheadernamescolname <- function(x) {
+  # interpret shorthand/aliases for column names of map_headernames
+  akas <- list(api = 'apiname',
+               csv = 'csvname2.2',
+               r =   'rname',
+               original = 'oldnames',
+               friendly = 'newnames_ejscreenapi',
+               long = 'longname_tableheader')
+  x[x %in% names(akas)] <- as.vector(unlist(akas[match(x[x %in% names(akas)], names(akas))]))
+  return(x)
+}
+#################################################################### # 
