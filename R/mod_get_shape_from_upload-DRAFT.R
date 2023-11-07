00d1aae7725990a37e1416b4e516a686d15573af
Mark Corrales <corrales.mark@epa.gov>
2023-11-07 14:24:04 -0500

    not using it, but edited/renamed draft module for uploading shapefile

--- R/mod_get_shape_from_upload-DRAFT.R
+++ R/mod_get_shape_from_upload-DRAFT.R
@@ -45,3 +45,97 @@ mod_get_shape_upload_server <- function(id){
     
 ## in the server
 # mod_get_shape_upload_server("get_shape_from_upload_1")
+
+#############################################################
+################################################ ################################################# #
+################################################ ################################################# #
+#
+# To test the module ####
+
+testing_this_module <- FALSE # so that installation will not source this and launch the module as a mini app
+if (testing_this_module) {
+  
+  #  (EJAM)
+  # lib x ra ry(   shiny); lib x ra ry(   magrittr); lib x rary(   leaflet)  # maybe? must attach all of those manually for this to work unless EJAM attached already?
+  source(system.file("global.R", package = "EJAMejscreenapi"))
+  
+  #  (  EJAM) # for testpoints_10, e.g., BUT THAT WOULD REPLACE AN UPDATED MODULE BELOW IF NOT ALREADY REBUILT/RELOADED WITH UPDATE
+  
+  default_calculate_ratios <- TRUE
+  use_ejscreenit_tf <- FALSE
+  ######################### #
+  TEST_UI <- function(request) {
+    shiny::fluidPage(
+      tabsetPanel(
+        tabPanel(
+          title = "api app",
+          shiny::h2('EJScreen API batch tool packaged with EJAM'), 
+          # verbatimTextOutput("testinfo1"), 
+          # verbatimTextOutput("testinfo_radius"),
+          # shiny::textOutput("testinfo2"),
+          # EJAM:::mod_ejscreenapi_ui("TESTID", simpleradius_default_for_ui = 2),
+          
+          EJAM:::mod_ejscreenapi_ui("TESTID", 
+                             
+                             simpleradius_default_for_ui = 2
+          ),
+          br()
+        ),
+        tabPanel(
+          title = "results in overall app", br(),
+          
+          h3("you could show them here"),
+          ## redundant since module already shows it, but here as example of module returning a table.
+          ##  module server function returns a table to overall server/app, which itself can then use/display that table. 
+          # DT::dataTableOutput("results"),
+          ## Demo of how you might send radius from outer app to module as starting value of module radius slider:
+          # sliderInput("outerappradius", label = "Radius we could preset", min = 1, max = 10, step = 1, value = 1.2),
+          br()
+        )))
+  }
+  ######################### #
+  TEST_SERVER <- function(input, output, session) {
+    
+    x <- EJAM:::mod_ejscreenapi_server(
+      
+      "TESTID", 
+      
+      default_points_shown_at_startup_react = reactive(testpoints_5[1:2,]), 
+      use_ejscreenit = use_ejscreenit_tf
+    )
+    
+    # output$testinfo1 <- renderPrint( ("info can go here "))
+    # output$testinfo_radius <- renderPrint(paste0("Radius is ", x()$radius.miles, " miles"))
+    # output$testinfo2 <- renderText(cat("x names:  ", paste0(names(x()), collapse = ", "), "\n"))
+    
+    output$results <- DT::renderDataTable({x()}, 
+                                          options = list(
+                                            selection = 'multiple',
+                                            dom = 'rtip', # specify 4 DOM elements: 
+                                            # processing, table, info, pagination 
+                                            # per https://datatables.net/examples/basic_init/dom.html
+                                            scrollX = TRUE, 
+                                            searchPanes = TRUE  # does this work?
+                                          ),
+                                          escape = FALSE
+    )
+    # *** CAUTION ***
+    # escape=TRUE is better for security reasons (XSS attacks).
+    # escape=FALSE lets ejscreen URL links work, 
+    #   but not links from ECHO table download.
+  }
+  ######################### #
+  
+  shinyApp(ui = TEST_UI, server = TEST_SERVER) # Try module in mini/test app  
+  
+  
+  # note you cannot wrap ui and server code in a single function when using modules 
+  
+  ##  in the main app UI
+  # mod_ejscreenapi_ui("x2")
+  
+  ##   in the main app server
+  # mod_ejscreenapi_server("x2") 
+}
+################################################ ################################################# #
+################################################ ################################################# #
