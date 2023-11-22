337cd43fa5bf7fcb9eb631540a438eda0591049c
Marschall Furman <51181259+mlfurman3@users.noreply.github.com>
2023-11-22 10:40:47 -0500

    move ridgeline plot to external function, plot_ridgeline_ratios

--- R/app_server.R
+++ R/app_server.R
@@ -1688,41 +1688,14 @@ app_server <- function(input, output, session) {
       
     } else if (input$plotkind_1pager == 'ridgeline') {
       
-      # https://r-graph-gallery.com/294-basic-ridgeline-plot.html#color
-      # https://r-graph-gallery.com/294-basic-ridgeline-plot.html#shape
-      #  (ggplot2)
-      #  (ggridges) # listed in DESCRIPTION file Imports
-      #  (viridis) # listed in DESCRIPTION file Imports
-      #  (hrbrthemes) # listed in DESCRIPTION file Imports
-      
       ## ratios by site  (demog each site / demog avg in US)
       ratio.to.us.d.bysite <- data_processed()$results_bysite[ ,  c(
         ..names_d_ratio_to_avg, 
         ..names_d_subgroups_ratio_to_avg
       )]
-      ## assign column names (could use left_join like elsewhere)
-      names(ratio.to.us.d.bysite) <-  c(
-        names_d_friendly, 
-        names_d_subgroups_friendly
-      ) # long_names_d$var_names[match( names_d_fixed, long_names_d$vars)] #names_d_fixed and long_names_d no longer exist. use names_d_friendly, etc.
-      ## pivot data from wide to long - now one row per indicator 
-      ratio.to.us.d.bysite <- ratio.to.us.d.bysite %>% 
-        tidyr::pivot_longer(cols = dplyr::everything(), names_to = 'indicator') %>% 
-        ## replace Infs with NAs - these happen when indicator at a site is equal to zero
-        dplyr::mutate(value = dplyr::na_if(value, Inf)) #%>% 
-      # NOTE NOW ratio.to.us.d.bysite IS A tibble, not data.frame, and is in LONG format now. !!!
       
-      # ridgeline Plot - need to adjust xlim so max is about a ratio of 3.0 (or less if none are >=3x)
-      ggplot(ratio.to.us.d.bysite, aes(x = `value`, y = `indicator`, fill = ..x..)) +
-        geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01) +
-        scale_fill_viridis(name = "Ratio to US Overall Value", option = "C") +
-        labs(title = 'Ratio to US Overall for each Demographic Indicator across these Sites') +
-        theme_ipsum() +
-        theme(
-          legend.position = "none",
-          panel.spacing = unit(0.1, "lines"),
-          strip.text.x = element_text(size = 8)
-        )
+      plot_ridgeline_ratios(ratio.to.us.d.bysite)
+                            
       
     } else {if (input$plotkind_1pager == "box") {
       
