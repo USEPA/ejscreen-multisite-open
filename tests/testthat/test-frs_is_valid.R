a92d41fa547a17d55bae5e45cd45f3290017e10e
Mark Corrales <corrales.mark@epa.gov>
2024-01-15 02:09:26 -0500

    frs_is_valid

--- tests/testthat/test-frs_is_valid.R
+++ tests/testthat/test-frs_is_valid.R
@@ -9,31 +9,61 @@
 
 # works with standard REGISTRY_ID column
 test_that('REGISTRY_ID column is recognized',{
-  expect_true(val <- frs_is_valid(data.frame("REGISTRY_ID" = 110009441979,
-                                             "PRIMARY_NAME" = "CAPITAL EXXON")))
-  })
+  expect_true(frs_is_valid(data.frame("REGISTRY_ID" = 110000307695,
+                                             "PRIMARY_NAME" = "testname")))
+})
 
 # works with regid column 
-# script frs_is_valid trys to handle "regid" but looks to replace "RegistryID"
 test_that('regid column is recognized',{
-  expect_true(val <- frs_is_valid(data.frame("regid" = 110009441979,
-                                                "PRIMARY_NAME" = "CAPITAL EXXON")))
+  expect_true(frs_is_valid(data.frame("regid" = 110000307695,
+                                             "PRIMARY_NAME" = "testname")))
 })
 
 # works with RegistryID column
-# script frs_is_valid trys to handle "siteid" twice. Was the first one supposed to 
-# look for "RegistryID" and this one to look for "regid"?
 test_that('RegistryID column is recognized',{
-  expect_true(val <- frs_is_valid(data.frame("RegistryID" = 110009441979,
-                                             "PRIMARY_NAME" = "CAPITAL EXXON")))
+  expect_true(frs_is_valid(data.frame("RegistryID" = 110000307695,
+                                             "PRIMARY_NAME" = "testname")))
 })
 
 # works with siteid column
-# regid, RegisttryID and siteid all have issues caused by 
-# frs_from_regid ( script name is frs_from_siteid ) returning an empty data frame
 test_that('siteid column is recognized',{
-  expect_true(val <- frs_is_valid(data.frame("siteid" = 110009441979,
-                                             "PRIMARY_NAME" = "CAPITAL EXXON")))
+  expect_true(frs_is_valid(data.frame("siteid" = 110000307695,
+                                             "PRIMARY_NAME" = "testname")))
 })
 
 # those are the four column names in frs_is_valid
+
+# fails if colname is something like "id"
+test_that("colname like 'id' fails to be recognized as regid since too ambiguous", {
+  testthat::expect_false(
+    frs_is_valid(data.frame(id = 110000307695, "PRIMARY_NAME" = "testname"))
+  )
+})
+########################### # 
+
+# gives FALSE if colname is valid other than REGISTRY_ID but no valid numbers in alias col used
+test_that("colname not REGISTRY_ID but seems to be ok alias, so check done for invalid numbers, so returns FALSE if invalid number", {
+  testthat::expect_false(
+    frs_is_valid(data.frame(regid = 1, "PRIMARY_NAME" = "testname"))
+  )
+})
+
+# Oddly, does not fail if colname is REGISTRY_ID but no valid numbers provided 
+test_that("colname REGISTRY_ID ok, so no check done for invalid numbers, so returns TRUE despite all bad numbers", {
+  testthat::expect_true(
+    frs_is_valid(data.frame(REGISTRY_ID = 1, "PRIMARY_NAME" = "testname"))
+  )
+})
+
+test_that("providing a data.table not just data.frame is handled/ fixed", {
+  testthat::expect_no_error(
+    frs_is_valid(data.table(id = 110000307695, "PRIMARY_NAME" = "testname"))
+  )
+})
+
+
+
+# could test...
+# if above tests but for 2+ rows
+# if 0 rows
+# if not a data.frame or data.table
