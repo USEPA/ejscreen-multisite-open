8ce19b411342bb7b32207e9c695346ed4a57cbf5
Mark Corrales <corrales.mark@epa.gov>
2024-01-15 02:04:54 -0500

    ask_number

--- R/utility_ask_number.R
+++ R/utility_ask_number.R
@@ -1,11 +1,11 @@
 #' utility - interactive prompt in RStudio to ask user to specify number like radius
 #' same as askradius()
 #' @aliases askradius
-#' @param default default value
-#' @param title question
-#' @param message title of popup dialog box
+#' @param default default value for the number to be provided and returned
+#' @param title title of popup dialog box, like "Radius"
+#' @param message question, like, "Within how many miles of each point?"
 #' @seealso askYesNo()
-#' @return a number
+#' @return a single number
 #'
 ask_number <- function(default = 3, title = "Radius", message = "Within how many miles of each point?") {
   radius <- NA
@@ -13,7 +13,8 @@ ask_number <- function(default = 3, title = "Radius", message = "Within how many
     radius <- as.numeric(rstudioapi::showPrompt(title = title, message = message, default = default))
     if (length(radius) == 0) {radius <- NA}
   }
-  cat("Specified value is", radius, '\n')
+  if (title == "Radius" & missing(message)) {units_text <- " miles."} else {units_text <- ""}
+  cat("Specified value is", radius, units_text, '\n')
   return(radius)
 }
 
