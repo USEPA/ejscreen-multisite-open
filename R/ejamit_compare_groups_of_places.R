ea89eca781efc79874b24a530f4cfb39ea73f6a8
Mark Corrales <corrales.mark@epa.gov>
2024-05-05 16:22:07 -0400

    small edit to ejamit_compare_types_of_places()

--- R/ejamit_compare_groups_of_places.R
+++ R/ejamit_compare_groups_of_places.R
@@ -3,7 +3,8 @@
 
 
 #' Compare subsets (types) of places that are all from one list
-#'
+#' *** DRAFT - may change output formats of results_bytype vs results_overall
+#' 
 #' @param sitepoints see [ejamit()]
 #' @param typeofsite   vector of length same as NROW(sitepoints), where each unique value defines a group of sites
 #' @param ...  see [ejamit()]
@@ -42,38 +43,43 @@ ejamit_compare_types_of_places <- function(sitepoints, typeofsite = NULL, ...) {
       ejamit(sitepoints = sitepoints[typeofsite == types[i], ], ..., quiet = TRUE, silentinteractive = TRUE)
     }) 
     
-    results_overall[[i]] <- out$results_overall
-    results_bysite[[i]] <- out$results_bysite
-    results_bybg_people[[i]] <- out$results_bybg_people
-    # results_summarized[[i]] <= out$results_summarized
+    results_overall[[i]] <- out$results_overall   # one row, for this group of places (this type) 
+    results_bysite[[i]] <- out$results_bysite    # one row per site, in this group
+    
+    results_bybg_people[[i]] <- out$results_bybg_people   # one row per blockgroup near this group of sites
+    # results_summarized[[i]] <= out$results_summarized  # this is a list of "rows" and "cols", 
+        # where cols is a table for this group of sites, 1 row per site in this group?. rows is a table of 1 col per indicator?
+    
     longnames[[i]] <- out$longnames
-    typeofsite_list[[i]] <- typeofsite[typeofsite == types[i]]
-    sitecount_bytype[[i]] <- NROW(results_bysite)
+    
+    typeofsite_list[[i]] <- typeofsite[typeofsite == types[i]]  # one item, name of this type of site
+    sitecount_bytype[[i]] <- NROW(results_bysite) # one number, count of sites in this group
   }
   cat("\n\n")
   
   out = list(
     
     # sitepoints = sitepoints, 
+    
     typeofsite = unlist(typeofsite_list),
     types = types,
     ejam_uniq_id = sitepoints$ejam_uniq_id,
-    
-    ## This seems inefficient to run them all AGAIN but as a whole instead of by group:
-    # results_overall = ejamit(sitepoints = sitepoints, ...)$results_overall,
-    
-    
     sitecount_bytype = unlist(sitecount_bytype),
     results_bytype = data.table::rbindlist(results_overall), 
     # will be N rows instead of usual 1 row (where N is the number of types of site, ie groups)
     
+    # results_overall = ejamit(sitepoints = sitepoints, ...)$results_overall,
+    ## This seems inefficient to run them all AGAIN but as a whole instead of by group:
+
     results_bysite = data.table::rbindlist(results_bysite),
     # lacks column with typeofsite (to stay same shape as normal ejamit()$results_bysite), and out$typeofsite has that info.
     
     # results_bybg_people  would be needed once per group to show x as func of distance within a typeofsite,
-    # except you cant rbind them since they overlap where 2 groups of sites share some blockgroups.
+    # bur cannot rbind them since they overlap where 2 groups of sites share some blockgroups.
+    ## not sure how/if to report this... *** tbd
     
-     # results_summarized <- format is different
+     # results_summarized <- 0, # format is different. ## not sure how/if to report this... *** tbd
+    
     
     longnames = longnames[[1]]
   )
