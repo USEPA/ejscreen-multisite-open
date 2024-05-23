d70c60c0b5288b6cc8bd2b433905093e155bb31a
Mark Corrales <corrales.mark@epa.gov>
2024-05-23 15:41:01 -0400

    began drafting tests of fixcolnames...

---
+++ tests/testthat/test-fixcolnames.R
@@ -0,0 +1,13 @@
+
+
+# 
+#   fixcolnames(c("RAW_D_INCOME", "S_D_LIFEEXP")) # assumes it was in original format as API outputs
+#   
+#   fixcolnames(c("RAW_D_INCOME", "S_D_LIFEEXP"), newtype = "longname")
+#   
+#   fixcolnames(c("resp","rsei"),  oldtype = "r", newtype = "longname")
+#   
+#   fixcolnames(c("resp","rsei"),  oldtype = "r", newtype = "api")
+#   
+#   fixcolnames(c("resp","pctlowinc"), oldtype = "r", newtype = "varlist")
+# 
