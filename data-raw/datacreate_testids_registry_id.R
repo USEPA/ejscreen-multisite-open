053c0e3205831e957d0903af6d6dc4b910f79261
Mark Corrales <corrales.mark@epa.gov>
2024-07-21 17:49:32 -0400

    Update datacreate_testids_registry_id.R

--- data-raw/datacreate_testids_registry_id.R
+++ data-raw/datacreate_testids_registry_id.R
@@ -1,41 +1,60 @@
+#################################### #
+## script to create dataset
 
-## code to prepare `testids_registry_id` dataset  
+#################################### #
+# check what was in INSTALLED package so far
+
+### INSTALLED PACKAGE lazy loaded data test objects - for registryid or programid
+x <- as.data.frame(data(package = "EJAM")$results)
+cat("Already installed: \n\n")
+print(x[grepl("id", x$Item), ])
+
+### INSTALLED PACKAGE (not source) test files - for registryid or programid
+# print( dir(base::system.file("testdata/registryid", package = "EJAM"), full.names = T) )
+# print( dir(base::system.file("testdata/programid", package = "EJAM"), full.names = T) )
+### if you dont specify base:: and you had done devtools::load_all() then it actually will look in the source package, not the installed package
+
+#################################### #
+# create the dataset
 
 testids_registry_id <- c(
   110071293460, 110070874073, 110070538057, 110044340807,
   110030509215, 110019033810, 110056111559, 110056982323
 )
+if (anyNA( frs_from_regid(testids_registry_id)$lat)) {stop("some of the testids_registry_id are not in the FRS database or lack lat,lon")}
+## or  
+# latlon_from_regid(testids_registry_id)
 
+## requires first load_all() or require() or EJAM::: to access the function
 testids_registry_id <- metadata_add(testids_registry_id)
 
+## compare the one in memory right now to the already-installed one:
+# all.equal(test_regid, EJAM::test_regid)
+all.equal(testids_registry_id, EJAM::testids_registry_id)
+# all.equal(testids_program_sys_id, EJAM::testids_program_sys_id)
+
+
 usethis::use_data(testids_registry_id, overwrite = TRUE)
- 
-#see  EJAM function    latlon_from_regid(testids_registry_id)
+
 
 #################################### #
-# check what is created
-
-# data creation scripts
-
-dir("./data-raw/", pattern = "datacreate_.*id")
+## check what was just created (or was already) in SOURCE package
 
 
-# lazy loaded data objects in R
+## test objects - for registryid or programid
 
-x = EJAM:::datapack("EJAM")
-x[grepl("id", x$Item), ]
+dir("./data/", pattern = ".*id.*rda$", ignore.case = T, full.names = T)
 
+# test files - for registryid or programid
 
-# installed test files
+cat("\n\n")
+dir("./inst/testdata/programid", recursive = TRUE, full.names = T)
+cat("\n\n")
+dir("./inst/testdata/registryid", recursive = TRUE, full.names = T)
+cat("\n\n")
 
-dir(system.file("testdata", package = "EJAM"))
-dir(system.file("testdata/registryid", package = "EJAM"))
-dir(system.file("testdata/programid", package = "EJAM"))
-
-
-# documentation files
+# documentation files - for registryid or programid
 
 dir('./R/', pattern = "data_.*id", ignore.case = T)
 
 #################################### #
-
