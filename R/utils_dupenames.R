8c7aa18dc9572f63ec0f3e43427a3744070778be
Mark Corrales <corrales.mark@epa.gov>
2024-08-14 15:02:15 -0400

    (just 2 utility functions)

--- R/utils_dupenames.R
+++ R/utils_dupenames.R
@@ -1,6 +1,7 @@
 
 # Functions here 
 
+# nacounts
 # setdiff_yx
 # setdiff2
 # unshared
@@ -12,6 +13,29 @@
 # functions_in_pkg
 # dependencies_of_ejam
 
+##################################################################### #
+nacounts = function(x, showall=FALSE) {
+  
+  if (NCOL(x) == 1) {
+          return(
+            data.frame(nas = sum(is.na(x)),
+                       other = sum(!is.na(x)))
+            ) # ignores showall
+  } else {
+    if (showall) {
+      shown <- rep(TRUE, NCOL(x))
+    } else {
+      shown <- sapply(x, anyNA)
+    }
+    z = data.frame(
+      nas = sapply(x, function(y) sum(is.na(y))), 
+      other = sapply(x, function(y) sum(!is.na(y)))
+    )
+    z = z[shown,]
+    return(z)
+  }
+}
+### e.g.,  nacounts(data.frame(a = 1:3, b= c(NA, NA, 0), d = c(NA,1,1)))
 ##################################################################### #
 
 
