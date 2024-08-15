d345d6c2837ffcc0a9f1c544f4c54fe055990671
Mark Corrales <corrales.mark@epa.gov>
2024-08-15 19:50:46 -0400

    update/reorg some unit tests and manual_nonalphabetical.R

--- tests/testthat/test-ejscreenRESTbroker-functions.R
+++ tests/testthat/test-ejscreenRESTbroker-functions.R
@@ -2,6 +2,22 @@
 # library(testthat)    
 #   test_local()  to manually run all tests on local source pkg
 
+
+testradius = 1
+testlat <-  38.8959  # testpoints_50$lat[1]
+testlon <- -77.02985 # testpoints_50$lon[1]
+test2lat <- c(33.943883,    39.297209)
+test2lon <- c(-118.241073, -76.641674)
+pts <- data.frame(lat = test2lat, lon = test2lon)
+
+outrest       <- ejscreenRESTbroker(lon = testlon, lat = testlat, radius = testradius)
+outrest2table <- ejscreenRESTbroker2table(outrest, getstatefromplacename = TRUE)
+out1          <- ejscreenapi1(lon = testlon,  lat = testlat, radius = testradius) # CAN SOMETIMES TAKE 30 SECONDS, SOMETIMES 5 SECONDS
+# out_api       <- ejscreenapi(lon = test2lon, lat = test2lat, radius = testradius, on_server_so_dont_save_files = TRUE, save_when_report = FALSE)
+brokerout <- try(ejscreenRESTbroker(lon = testpoints_5$lon[1], lat = testpoints_5$lat[1], radius = testradius))
+missing_api_results <- inherits(brokerout, "try-error")
+
+
 ################################ # ########################## # ejscreenRESTbroker ####
 
 cat('\ntesting ejscreenRESTbroker\n')
@@ -20,21 +36,21 @@ testthat::test_that("ejscreenRESTbroker() returns ok status code 200 and an elem
 })
 test_that("ejscreenRESTbroker() output class() is still identical to that of saved testoutput_ejscreenRESTbroker_1pts_1miles" , {
   skip_if(missing_api_results, "skipping because ejscreenRESTbroker() failed")
-  expect_equal( class(x), class(testoutput_ejscreenRESTbroker_1pts_1miles))
+  expect_equal( class(brokerout), class(testoutput_ejscreenRESTbroker_1pts_1miles))
 })
 test_that("ejscreenRESTbroker() output names() are still identical to those of saved testoutput_ejscreenRESTbroker_1pts_1miles", {
   skip_if(missing_api_results, "skipping because ejscreenRESTbroker() failed")
-  expect_equal(names(testoutput_ejscreenRESTbroker_1pts_1miles), names(x))
+  expect_equal(names(testoutput_ejscreenRESTbroker_1pts_1miles), names(brokerout))
 })
 test_that("ejscreenRESTbroker() output $headers$`content-type` is still identical to those of saved testoutput_ejscreenRESTbroker_1pts_1miles", {
   skip_if(missing_api_results, "skipping because ejscreenRESTbroker() failed")
-  expect_equal(x$headers$`content-type`, testoutput_ejscreenRESTbroker_1pts_1miles$headers$`content-type`)
+  expect_equal(brokerout$headers$`content-type`, testoutput_ejscreenRESTbroker_1pts_1miles$headers$`content-type`)
 })
 test_that("ejscreenRESTbroker() output $headers$`content-type` is still identical to those of saved testoutput_ejscreenRESTbroker_1pts_1miles", {
   skip_if(missing_api_results, "skipping because ejscreenRESTbroker() failed")
-  expect_equal(class(x$content), class(testoutput_ejscreenRESTbroker_1pts_1miles$content))
+  expect_equal(class(brokerout$content), class(testoutput_ejscreenRESTbroker_1pts_1miles$content))
 })
-# expect_equal(x$content, testoutput_ejscreenRESTbroker_1pts_1miles$content)
+# expect_equal(brokerout$content, testoutput_ejscreenRESTbroker_1pts_1miles$content)
 
 
 
--- tests/testthat/test-ejscreenapi_plus.R
+++ tests/testthat/test-ejscreenapi_plus.R
@@ -1,6 +1,23 @@
 ################################ # ################################ # ejscreenapi_plus ####
 cat('\ntesting ejscreenapi_plus()\n')
 
+
+testradius = 1
+testlat <-  38.8959  # testpoints_50$lat[1]
+testlon <- -77.02985 # testpoints_50$lon[1]
+test2lat <- c(33.943883,    39.297209)
+test2lon <- c(-118.241073, -76.641674)
+# pts <- data.frame(lat = test2lat, lon = test2lon)
+# 
+# outrest       <- ejscreenRESTbroker(lon = testlon, lat = testlat, radius = testradius)
+# outrest2table <- ejscreenRESTbroker2table(outrest, getstatefromplacename = TRUE)
+# out1          <- ejscreenapi1(lon = testlon,  lat = testlat, radius = testradius) # CAN SOMETIMES TAKE 30 SECONDS, SOMETIMES 5 SECONDS
+# # out_api       <- ejscreenapi(lon = test2lon, lat = test2lat, radius = testradius, on_server_so_dont_save_files = TRUE, save_when_report = FALSE)
+# brokerout <- try(ejscreenRESTbroker(lon = testpoints_5$lon[1], lat = testpoints_5$lat[1], radius = testradius))
+# missing_api_results <- inherits(brokerout, "try-error")
+# 
+# 
+
 test_that("ejscreenapi_plus() works at all", {
   expect_no_error(
     suppressWarnings({
