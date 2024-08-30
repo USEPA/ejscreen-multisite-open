3a6b1fff5b059af47d8b6b10c3d9b5e1f8a947da
Mark Corrales <corrales.mark@epa.gov>
2024-08-29 23:29:10 -0400

    map_google() added - provides hyperlinks to google maps

--- R/MAP_FUNCTIONS.R
+++ R/MAP_FUNCTIONS.R
@@ -665,3 +665,31 @@ mapfast_gg <- function(mydf=data.frame(lat = 40, lon = -100)[0,],
 #
 ############################ #
 
+
+#' Map - Open Google Maps in browser
+#'
+#' @param lat - Anything that can be handled by [sitepoints_from_any()].
+#'   Leave unspecified to interactively browse to a .xlsx file that has lat,lon columns,
+#'   or lat can be a data.frame with lat,lon column names in which case longitude should not be provided,
+#'   such as lat = testpoints[1,], or lat and lon can be separately provided as vectors.
+#' @param lon longitude, or omit this parameter to provide points as the first parameter.
+#' 
+#' @param zoom zoomed out value could be 3 or 5, zoomed in default is 12
+#' @param launch logical, whether to launch browser 
+#' @return opens a browser window with Google Maps centered on the specified lat, lon
+#' @examples # map_google(testpoints_10[1,])
+#' 
+#' @export
+#'
+map_google <- function(lat, lon, zoom = 12, point = TRUE, launch = TRUE) {
+  
+  urls = url_map_google(lat = lat, lon = lon, zoom = zoom, point = point)
+  if (launch) {
+    if (length(urls) > 1) {
+      message("browsing to only the first URL out of ", length(urls))
+    }
+    browseURL(urls[1])
+  }
+  urls
+}
+############################ #
