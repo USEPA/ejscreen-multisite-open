d5abb1732f78bfafe8c0c23e4ccaf4fe86b6b50b
ejanalysis <ejanalysis@users.noreply.github.com>
2024-09-16 10:28:24 -0400

    fix typo in message in read_csv_or_xl.R

--- R/read_csv_or_xl.R
+++ R/read_csv_or_xl.R
@@ -44,7 +44,7 @@ read_csv_or_xl <- function(fname=NULL, show_col_types=FALSE, rowsize_warn= 30 * 
   if (grepl('\\.csv$', fname, ignore.case = TRUE)) {
     filecontents <- try(as.data.frame(readr::read_csv(file = fname, show_col_types = show_col_types)))
     if(inherits(filecontents, "try-error")){
-      message("Erorr reading CSV file, returning NULL")
+      message("Error reading CSV file, returning NULL")
       return(NULL)
     }
     # error handling could go here - to ensure it is a solid block of data, only 1 header row, no extra rows or cols 
@@ -52,7 +52,7 @@ read_csv_or_xl <- function(fname=NULL, show_col_types=FALSE, rowsize_warn= 30 * 
   if (grepl('\\.xls$|\\.xlsx$', fname, ignore.case = TRUE)) {
     filecontents <- try(as.data.frame(readxl::read_excel(path = fname, sheet = 1)))
     if(inherits(filecontents, "try-error")){
-      message("Erorr reading excel file, returning NULL")
+      message("Error reading excel file, returning NULL")
       return(NULL)
     }
     # error handling could go here- to ensure it is a solid block of data, only 1 header row, no extra rows or cols or merged cells
