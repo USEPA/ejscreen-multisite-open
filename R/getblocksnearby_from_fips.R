12529a40cb6a8afb385589681e266259dc5d73e8
Mark Corrales <corrales.mark@epa.gov>
2024-09-19 19:17:40 -0400

    trying to speed up getblocksnearby_from_fips ...
    most of the speedup is avoiding loading bgid2fips or blockid2fips which makes the first time much faster
    comparing about 3 combos for speed

--- R/getblocksnearby_from_fips.R
+++ R/getblocksnearby_from_fips.R
@@ -57,14 +57,14 @@ getblocksnearby_from_fips <- function(fips, inshiny = FALSE, need_blockwt = TRUE
   }
   suppressWarnings({ # because if length was 1 and added NA at end, this reports irrelevant warning
   ## create two-column dataframe with bgs (values) and original fips (ind)
-  # fips_bg_from_anyfips() returns all blockgroup fips codes contained within each fips provided
-  # fips_bgs_in_fips() replaces fips_bg_from_anyfips()
-    all_bgs <- stack(sapply(fips_vec, fips_bgs_in_fips))
-  # all_bgs <- stack(sapply(fips_vec, fips_bg_from_anyfips)) # Slow:  1.4 seconds for all counties in region 6, e.g.
+  # fips_bgs_in_fips1() returns all blockgroup fips codes contained within each fips provided
+  # fips_bgs_in_fips() replaces fips_bgs_in_fips1()
+    # all_bgs <- stack(sapply(fips_vec, fips_bgs_in_fips)) # newer - fast alone but slow in sapply?
+  all_bgs <- stack(sapply(fips_vec, fips_bgs_in_fips1)) # Slow:  1.4 seconds for all counties in region 6, e.g.
   })
   names(all_bgs) <- c('bgfips', 'ejam_uniq_id')
   
-  # *** It actually could be more efficient to replace the above fips_bg_from_anyfips() 
+  # *** It actually could be more efficient to replace the above fips_bgs_in_fips1() 
   # or make a new func to provide bgid_from_anyfips() 
   # instead of 1st getting bgfips and then needing to look up bgid by bgfips - 
   #
