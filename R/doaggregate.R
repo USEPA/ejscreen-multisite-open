1fa747dbf54dba810d199f9f3379e9bf25055ed6
dougmoy <douglashtmoy@gmail.com>
2024-10-08 15:41:52 -0400

    set drinking to be NA in doaggregate.R

--- R/doaggregate.R
+++ R/doaggregate.R
@@ -763,6 +763,8 @@ doaggregate <- function(sites2blocks, sites2states_or_latlon=NA,
   ## >>> TEMPORARY PATCH UNTIL FORMULA FIXED - SEE ISSUE #498  https://github.com/USEPA/EJAM/issues/498 ####
   results_overall$pctownedunits <- NA; results_bysite$pctownedunits <- NA
   
+  results_overall$drinking <- NA; results_bysite$drinking <- NA
+  
   
   ############################################### #
   ##     later, for results_overall, will calc state pctiles once we have them for each site
