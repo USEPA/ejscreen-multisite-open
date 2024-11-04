5f61f4804c8beeddf5565af561ab8f5d4d5e7866
Mark Corrales <corrales.mark@epa.gov>
2024-11-04 15:27:09 -0500

    Update TEST COVERAGE REPORT.R

--- tests/TEST COVERAGE REPORT.R
+++ tests/TEST COVERAGE REPORT.R
@@ -1,52 +1,69 @@
-# !diagnostics off
-##   disables diagnostics within this document
+# !diagnostics off   ## would  disable diagnostics within this document
 
-if (1 == 0) { # so it does not do anything if source() is done by testing 
+coverage_check = function()   {
   
-# # Needed only for this script:
-  # 
+  # fs pkg is needed only for this script (but could be replaced by base or utils functions?) 
   library(fs)
-  library(tidyverse)
-
-tdat = bind_rows(
-  tibble(
-    type = "R",
-    path = dir_ls("R/", regexp = "\\.[Rr]$"),
-    name = as.character(path_ext_remove(path_file(path))),
-  ),
-  tibble(
-    type = "test",
-    path = dir_ls("tests/testthat/", regexp = "/test[^/]+\\.[Rr]$"),
-    name = as.character(path_ext_remove(str_remove(path_file(path), "^test[-_]"))),
-  )
-) %>%
-  pivot_wider(names_from = type, values_from = path) 
-
-tdat %>%   print(n = Inf)
-
-################################ # 
-
-# see all TEST FILES that were not named based on a .R file, 
-#   making it hard to know which .R files really lack tests
-
-tdat[!is.na(tdat$test) & is.na(tdat$R),  ] |> print(n = 500)
- 
-
-################################ # 
-
-# see all .R files that lack a test file with exactly matching name
-
-tdat[is.na(tdat$test) & !is.na(tdat$R) & "data_" != substr(tdat$name, 1,5), ] |> print(n = 500)
- 
-
-################################ # 
-
-
-# MATCHED EXACTLY -- see all test files that exactly match name of a .R file
-
-
-tdat[!is.na(tdat$test) & !is.na(tdat$R), c("R", "test")] |> print(n = 500)
-
-
-
+  require(tidyverse)
+  
+  tdat = bind_rows(
+    tibble(
+      type = "R",
+      path = dir_ls("R/", regexp = "\\.[Rr]$"),
+      name = as.character( fs::path_ext_remove(path_file(path))),
+    ),
+    tibble(
+      type = "test",
+      path = fs::dir_ls("tests/testthat/", regexp = "/test[^/]+\\.[Rr]$"),
+      name = as.character( fs::path_ext_remove(str_remove( fs::path_file(path), "^test[-_]"))),
+    )
+  ) %>%
+    pivot_wider(names_from = type, values_from = path) 
+  ################################ # 
+  cat("\n\nCOVERAGE CHECK \n\n")
+  # tdat %>%   print(n = Inf) # to see everything
+  ################################ # 
+  
+  cat(' -----------------------------------------------
+  
+  All test files that were not named based on a .R file, 
+     making it hard to know which .R files really lack tests:\n\n')
+  
+  tdat[!is.na(tdat$test) & is.na(tdat$R),  ] |> print(n = 500)
+  ################################ # 
+  
+  cat(" -----------------------------------------------
+  
+      All .R files that lack a test file with exactly matching name:\n\n")
+  
+  tdat[is.na(tdat$test) & !is.na(tdat$R) & "data_" != substr(tdat$name, 1,5), ] |> print(n = 500)
+  ################################ # 
+  
+  cat(' -----------------------------------------------
+  
+      MATCHED EXACTLY -- all test files that exactly match name of a .R file: \n\n')
+  
+  tdat[!is.na(tdat$test) & !is.na(tdat$R), c("R", "test")] |> print(n = 500)
+  ################################ # 
+  cat('
+      -----------------------------------------------\n\n')
+  
+  invisible(tdat)
 }
+
+
+## to use this function:
+#
+#  tdat <-  coverage_check() # to get report and see key info
+#  tdat %>%   print(n = Inf) # to see everything
+
+
+
+## also see 
+
+# y = EJAM:::functions_in_pkg('EJAM')
+
+## and 
+
+# x = analyze.stuff::linesofcode('./..', packages = 'EJAM') 
+
