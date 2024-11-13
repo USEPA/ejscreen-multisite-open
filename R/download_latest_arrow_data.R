b17ca439e24e164ff1ca7f73233de95efcaf8ff4
alex-silverman <alexsilverman96@gmail.com>
2024-11-13 09:22:34 -0500

    - fixes to reference app_sys - adding logging messages - other fixes to handle NULL/empties - only need to call pb_download once, on list of files, rather than in a loop - use readLines/writeLines to read/update the user's stored version of ejamdata.

--- R/download_latest_arrow_data.R
+++ R/download_latest_arrow_data.R
@@ -24,37 +24,41 @@ download_latest_arrow_data <- function(
   
   # get latest Arrow version (from EJAMData repo's latest release tag) 
   # and user's Arrow version (from DESCRIPTION's ArrowVersion attribute)
-  ejamdata_version_fpath <- paste0(app.sys('data'),"ejamdata_version.txt")
-  if(file.exists(ejamdata_version_fpath)) {
+  latestArrowVersion <- piggyback::pb_releases(repo = "USEPA/ejamdata")[1, "tag_name"]
+  ejamdata_version_fpath <- paste0(EJAM:::app_sys('data'),"/ejamdata_version.txt")
+  if(!file.exists(ejamdata_version_fpath)) {
     usersArrowVersions <- NULL
   } else {
-    usersArrowVersions <- readChar(ejamdata_version_fpath, file.info(ejamdata_version_fpath)$size)
+    usersArrowVersions <- readLines(ejamdata_version_fpath)
   }
 
+  filenames <- paste0(varnames, ".arrow")
+  
   # if user has latest release, check if any requested files are missing
-  # if so, need to re-download. Otherwise, all set
-  if(usersArrowVersions == latestArrowVersion) {
-    filenames <- paste0(varnames, ".arrow")
-    full_paths <- file.path(app.sys('data'), filenames)
+  # if so, need to re-download (default to all files). Otherwise, all set
+  if(isTRUE(usersArrowVersions == latestArrowVersion)) {
+    message("Arrow data is up-to-date!")
+    full_paths <- file.path(EJAM:::app_sys('data'), filenames)
     missing_files <- filenames[!file.exists(full_paths)]
-    if(length(missing_files) == 0) return
+    if(length(missing_files) == 0) return(NULL)
+    
+    message("However, some files are missing. Downloading them from EJAMData.")
   } else {
+    message("Arrow data is out-of-date!")
     missing_files <- filenames
   }
 
   # otherwise, download the data from EJAM's release assets
-  lapply(
-    missing_files, 
-    function(f) {
-      piggyback::pb_download(
-        file = f,
-        dest = app.sys('data'),
-        repo = 'USEPA/EJAM', 
-        tag = "latest"
-      )
-    }
+  piggyback::pb_download(
+    file = missing_files,
+    dest = EJAM:::app_sys('data'),
+    repo = 'USEPA/ejamdata', 
+    tag = "latest",
+    use_timestamps = FALSE
   )
 
+  message("Finished downloading. Updating stored EJAMData version.")
   # update user's arrowversion
-  writeChar(latestArrowVersion, ejamdata_version_fpath, nchars = nchar(latestArrowVersion), eos = NULL)
+  writeLines(latestArrowVersion, ejamdata_version_fpath)
+  message("Finished updating stored EJAMData version.")
 }
