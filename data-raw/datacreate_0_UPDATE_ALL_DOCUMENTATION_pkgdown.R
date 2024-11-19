4b3b807ce9287a8af89ac85823860dd26ab4298a
Mark Corrales <corrales.mark@epa.gov>
2024-11-19 10:59:37 -0500

    PKGDOWN Update

--- data-raw/datacreate_0_UPDATE_ALL_DOCUMENTATION_pkgdown.R
+++ data-raw/datacreate_0_UPDATE_ALL_DOCUMENTATION_pkgdown.R
@@ -12,7 +12,7 @@ offline_cat()
 
 # defaults
 dotests       = FALSE
-dodocument    = TRUE  # in case we just edited help or exports or func names !
+dodocument    = FALSE # in case we just edited help or exports or func names !
 doinstall     = FALSE # but skips document and vignettes
 doloadall_not_library = TRUE  # (happens after install if that is being done here)
 dopreviewonly = TRUE  # always.  do  pkgdown::build_site()  not  pkgdown::build_site_github_pages
